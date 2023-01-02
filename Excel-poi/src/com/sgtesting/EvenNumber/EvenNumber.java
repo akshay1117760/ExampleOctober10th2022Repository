1a27ccac3d5c1a07e196640d6e0e18d96bfe5e33
akshay1117760 <akpujari.117760@gmail.com>
2023-01-02 17:18:43 +0530

    EvenNumber

---
+++ Excel-poi/src/com/sgtesting/EvenNumber/EvenNumber.java
@@ -0,0 +1,17 @@
+package com.sgtesting.EvenNumber;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<=100; i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("Even number:"+i);
+			}
+		}
+
+	}
+
+}
